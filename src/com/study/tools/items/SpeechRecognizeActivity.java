82ec328793fc238afa3a61df5d68f8f6e88c5c02
lsyiverson <lsyiverson@gmail.com>
2012-12-17 18:00:58 +0800

    Modify the dialog to notice user about download the voice search app.

--- src/com/study/tools/items/SpeechRecognizeActivity.java
+++ src/com/study/tools/items/SpeechRecognizeActivity.java
@@ -4,7 +4,11 @@ package com.study.tools.items;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
@@ -18,6 +22,7 @@ import com.study.tools.R;
 public class SpeechRecognizeActivity extends Activity {
     private Button mBtnSpeak;
     private AutoCompleteTextView mTextView;
+    private final static int START_SPEECH_RECOGNIZE = 0x1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class SpeechRecognizeActivity extends Activity {
     private void setupViews(){
         mBtnSpeak = (Button)findViewById(R.id.btn_speak);
         mBtnSpeak.setOnClickListener(new OnClickListener() {
+            @Override
             public void onClick(View arg0) {
                 startSpeechRecognize();
             }
@@ -40,12 +46,50 @@ public class SpeechRecognizeActivity extends Activity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "test");
-        startActivityForResult(intent, 0x1111);
+        try {
+            startActivityForResult(intent, START_SPEECH_RECOGNIZE);
+        } catch (ActivityNotFoundException ex){
+            new AlertDialog.Builder(this).setTitle(R.string.no_voice_search_title)
+            .setMessage(R.string.no_voice_search_msg)
+            .setNegativeButton(R.string.no_voice_search_cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            })
+            .setPositiveButton(R.string.no_voice_search_download, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent installIntent = new Intent(Intent.ACTION_VIEW);
+                    installIntent.setData(Uri.parse("market://details?id=com.google.android.voicesearch"));
+                    try {
+                        startActivity(installIntent);
+                    } catch (ActivityNotFoundException ex) {
+                        dialog.dismiss();
+                        new AlertDialog.Builder(SpeechRecognizeActivity.this)
+                        .setTitle(R.string.no_market_title)
+                        .setMessage(R.string.no_market_msg)
+                        .setNegativeButton(R.string.no_market_cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .setPositiveButton(R.string.no_market_download, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW);
+                                intent.setData(Uri.parse("http://m.wandoujia.com/apps/com.google.android.voicesearch"));
+                                startActivity(intent);
+                            }
+                        }).show();
+                    }
+                }
+            }).show();
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 0x1111 && resultCode == RESULT_OK) {
+        if (requestCode == START_SPEECH_RECOGNIZE && resultCode == RESULT_OK) {
             ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, matches);
             mTextView.setAdapter(adapter);
